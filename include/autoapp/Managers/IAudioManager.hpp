17d3ea2b421f112e0fe83685ac2d62dbc27c2f76
silverchris <silverchris@gmail.com>
2022-08-09 12:07:00 -0400

    Add some documentation

--- include/autoapp/Managers/IAudioManager.hpp
+++ include/autoapp/Managers/IAudioManager.hpp
@@ -23,14 +23,24 @@ class IAudioManager: public IManager{
   virtual void start() = 0;
   virtual void stop() = 0;
 
+  /// Request the given audio focus type for the given channel id
+  /// \param channelId AASDK channel ID of affected channel
+  /// \param focus type of audio focus requested
   virtual void requestFocus(aasdk::messenger::ChannelId channelId, aasdk::proto::enums::AudioFocusType_Enum focus) = 0;
+
+  /// Release the audio focus for the channel ID provided. Release all if channel id is aasdk::messenger::ChannelID::None
+  /// \param channelId AASDK channel ID of affected channel
   virtual void releaseFocus(aasdk::messenger::ChannelId channelId) = 0;
 
-
+  /// Register callback for audio focus changes
+  /// \param callback function to be called on audio focus changes
   void registerFocusCallback(const focusCallback &callback) {
     focusCallbacks.emplace_back(callback);
   };
 
+  /// Call audio focus callbacks to notify of audio focus changes
+  /// \param channelId AASDK channel ID of affected channel
+  /// \param focus Audio focus state
   void updateFocus(aasdk::messenger::ChannelId channelId, aasdk::proto::enums::AudioFocusState_Enum focus) {
     for (auto &callback : focusCallbacks) {
       callback(channelId, focus);
--- include/autoapp/Managers/IVideoManager.hpp
+++ include/autoapp/Managers/IVideoManager.hpp
@@ -19,13 +19,20 @@ class IVideoManager: public IManager{
   virtual void start() = 0;
   virtual void stop() = 0;
 
+  /// Requests video focus from the VideoManager
   virtual void requestFocus() = 0;
+
+  /// Releases video focus
   virtual void releaseFocus() = 0;
 
+  /// Register for video focus change events
+  /// \param callback to be called when focus changes
   void registerFocus(const focusCallback &callback) {
     focusCallbacks.emplace_back(callback);
   };
 
+  /// Sends video focus status to all listeners
+  /// \param hasFocus true if video in focus, false otherwise
   void focusChanged(bool hasFocus) {
     for (auto &callback : focusCallbacks) {
       callback(hasFocus);
