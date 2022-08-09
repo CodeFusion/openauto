35ea19c412c56c3f468c64cd1e8b7a2e2f42a5d0
silverchris <silverchris@gmail.com>
2022-08-09 11:46:25 -0400

    VideoManager now updates everyone else's focus

--- include/Platforms/Mazda/Managers/VideoManager.hpp
+++ include/Platforms/Mazda/Managers/VideoManager.hpp
@@ -12,40 +12,42 @@
 
 class VideoManager: public IVideoManager{
  private:
+  std::shared_ptr<DBus::Connection> dbusConnection;
   std::shared_ptr<com_jci_bucpsa_objectProxy> bucpsa;
+  sigc::connection displayModeConnection;
   bool waitsForFocus = false;
-  bool currentDisplayMode;
+  bool currentDisplayMode = false;
   bool hasFocus = false;
   std::shared_ptr<com_jci_nativeguictrl_objectProxy> gui;
   void DisplayMode(uint32_t);
 
  public:
-  explicit VideoManager(const std::shared_ptr<DBus::Connection> &);
-  ~VideoManager();
+  explicit VideoManager(std::shared_ptr<DBus::Connection> session_connection);
+  ~VideoManager() override;
 
-  void start() override {};
-  void stop() override {};
+  void start() override;
+  void stop() override;
 
   void requestFocus() override;
   void releaseFocus() override;
 
   enum SURFACES {
-    NNG_NAVI_ID = 0,
-    TV_TOUCH_SURFACE = 1,
-    NATGUI_SURFACE = 2,
-    LOOPLOGO_SURFACE = 3,
-    TRANLOGOEND_SURFACE = 4,
-    TRANLOGO_SURFACE = 5,
-    QUICKTRANLOGO_SURFACE = 6,
-    EXITLOGO_SURFACE = 8,
-    JCI_OPERA_PRIMARY = 9,
-    JCI_OPERA_SECONDARY = 10,
-    lvdsSurface = 11,
-    SCREENREP_IVI_NAME = 12,
-    NNG_NAVI_MAP1 = 13,
-    NNG_NAVI_MAP2 = 14,
-    NNG_NAVI_HMI = 15,
-    NNG_NAVI_TWN = 16,
+    NNG_NAVI_ID [[maybe_unused]] = 0,
+    TV_TOUCH_SURFACE [[maybe_unused]] = 1,
+    NATGUI_SURFACE [[maybe_unused]] = 2,
+    LOOPLOGO_SURFACE [[maybe_unused]] = 3,
+    TRANLOGOEND_SURFACE [[maybe_unused]] = 4,
+    TRANLOGO_SURFACE [[maybe_unused]] = 5,
+    QUICKTRANLOGO_SURFACE [[maybe_unused]] = 6,
+    EXITLOGO_SURFACE [[maybe_unused]] = 8,
+    JCI_OPERA_PRIMARY [[maybe_unused]] = 9,
+    JCI_OPERA_SECONDARY [[maybe_unused]] = 10,
+    lvdsSurface [[maybe_unused]] = 11,
+    SCREENREP_IVI_NAME [[maybe_unused]]= 12,
+    NNG_NAVI_MAP1 [[maybe_unused]] = 13,
+    NNG_NAVI_MAP2 [[maybe_unused]] = 14,
+    NNG_NAVI_HMI [[maybe_unused]]= 15,
+    NNG_NAVI_TWN [[maybe_unused]] = 16,
   };
 
 };
--- src/Platforms/Mazda/Managers/VideoManager.cpp
+++ src/Platforms/Mazda/Managers/VideoManager.cpp
@@ -1,15 +1,9 @@
 #include "Platforms/Mazda/Managers/VideoManager.hpp"
 #include "easylogging++.h"
 
-VideoManager::VideoManager(const std::shared_ptr<DBus::Connection> &session_connection){
-  gui =
-      com_jci_nativeguictrl_objectProxy::create(session_connection, "com.jci.nativeguictrl", "/com/jci/nativeguictrl");
-  bucpsa = com_jci_bucpsa_objectProxy::create(session_connection, "com.jci.bucpsa", "/com/jci/bucpsa");
-  bucpsa->getcom_jci_bucpsaInterface()->signal_DisplayMode()->connect(sigc::mem_fun(*this, &VideoManager::DisplayMode));
+VideoManager::VideoManager(std::shared_ptr<DBus::Connection> session_connection) : dbusConnection(std::move(
+    session_connection)) {
 
-  std::tuple<unsigned int, int> display_mode = bucpsa->getcom_jci_bucpsaInterface()->GetDisplayMode();
-
-  currentDisplayMode = (bool) std::get<0>(display_mode);
 }
 
 void VideoManager::DisplayMode(uint32_t DisplayMode) {
@@ -26,30 +20,52 @@ void VideoManager::DisplayMode(uint32_t DisplayMode) {
 }
 
 void VideoManager::requestFocus() {
-  if (currentDisplayMode) {
-    // we can safely exit - backup camera will notice us when we finish; and we request focus back
-    waitsForFocus = true;
-    return;
+  if(gui) {
+    if (currentDisplayMode) {
+      // we can safely exit - backup camera will notice us when we finish; and we request focus back
+      waitsForFocus = true;
+      return;
+    }
+    if (waitsForFocus) {
+      // need to wait for a second (maybe less but 100ms is too early) to make sure
+      // the CMU has already changed the surface from backup camera to opera
+      sleep(1);
+      waitsForFocus = false;
+    }
+    LOG(DEBUG) << "Setting focus";
+    hasFocus = true;
+    gui->getcom_jci_nativeguictrlInterface()->SetRequiredSurfaces(std::to_string(SURFACES::TV_TOUCH_SURFACE), 1);
+    focusChanged(true);
   }
-  if (waitsForFocus) {
-    // need to wait for a second (maybe less but 100ms is too early) to make sure
-    // the CMU has already changed the surface from backup camera to opera
-    sleep(1);
-    waitsForFocus = false;
-  }
-  LOG(DEBUG) << "Setting focus";
-  hasFocus = true;
-  gui->getcom_jci_nativeguictrlInterface()->SetRequiredSurfaces(std::to_string(SURFACES::TV_TOUCH_SURFACE), 1);
 }
 
 void VideoManager::releaseFocus() {
-  LOG(DEBUG) << "Releasing focus";
-  hasFocus = false;
-  gui->getcom_jci_nativeguictrlInterface()->SetRequiredSurfaces(std::to_string(SURFACES::JCI_OPERA_PRIMARY), 1);
+  if(gui) {
+    LOG(DEBUG) << "Releasing focus";
+    hasFocus = false;
+    gui->getcom_jci_nativeguictrlInterface()->SetRequiredSurfaces(std::to_string(SURFACES::JCI_OPERA_PRIMARY), 1);
+    focusChanged(false);
+  }
 }
 
-VideoManager::~VideoManager() {
+void VideoManager::start() {
+  gui =
+      com_jci_nativeguictrl_objectProxy::create(dbusConnection, "com.jci.nativeguictrl", "/com/jci/nativeguictrl");
+  bucpsa = com_jci_bucpsa_objectProxy::create(dbusConnection, "com.jci.bucpsa", "/com/jci/bucpsa");
+  displayModeConnection = bucpsa->getcom_jci_bucpsaInterface()->signal_DisplayMode()->connect(sigc::mem_fun(*this,
+                                                                                                            &VideoManager::DisplayMode));
+
+  std::tuple<unsigned int, int> display_mode = bucpsa->getcom_jci_bucpsaInterface()->GetDisplayMode();
+
+  currentDisplayMode = (bool) std::get<0>(display_mode);
+}
+
+void VideoManager::stop() {
   LOG(DEBUG) << "Stopping VideoManager";
   releaseFocus();
+  displayModeConnection.disconnect();
   bucpsa.reset();
+  gui.reset();
 }
+
+VideoManager::~VideoManager() = default;
