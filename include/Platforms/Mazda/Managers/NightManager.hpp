cd6f302de3dfbe721cdeada69164ae4793258776
silverchris <silverchris@gmail.com>
2022-08-13 19:54:57 -0400

    make delay const

--- include/Platforms/Mazda/Managers/NightManager.hpp
+++ include/Platforms/Mazda/Managers/NightManager.hpp
@@ -20,7 +20,7 @@ class NightManager: public INightManager{
  private:
   asio::basic_waitable_timer<std::chrono::steady_clock> timer;
   std::mutex timerMutex;
-  int delay = 1000;
+  const int delay = 1000;
 
   void checkNight(const asio::error_code &error);
 
